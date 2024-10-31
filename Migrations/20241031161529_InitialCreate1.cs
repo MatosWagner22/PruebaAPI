47453c75d8eef18c2ef60adece990a64fba60073
MatosWagner22 <matoswagner22@gmail.com>
2024-10-31 15:40:03 -0400

    Completado Crud con API y Consumo API Externa

---
+++ Migrations/20241031161529_InitialCreate1.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PruebaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Descripcion",
+                table: "Peliculas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EsAnimada",
+                table: "Peliculas",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EsEstreno",
+                table: "Peliculas",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genero",
+                table: "Peliculas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UrlVideo",
+                table: "Peliculas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descripcion",
+                table: "Peliculas");
+
+            migrationBuilder.DropColumn(
+                name: "EsAnimada",
+                table: "Peliculas");
+
+            migrationBuilder.DropColumn(
+                name: "EsEstreno",
+                table: "Peliculas");
+
+            migrationBuilder.DropColumn(
+                name: "Genero",
+                table: "Peliculas");
+
+            migrationBuilder.DropColumn(
+                name: "UrlVideo",
+                table: "Peliculas");
+        }
+    }
+}
--- Models/Pelicula.cs
+++ Models/Pelicula.cs
@@ -1,3 +1,7 @@
+
+
+using System.ComponentModel.DataAnnotations;
+
 namespace PruebaAPI
 {
     public class Pelicula
@@ -5,6 +9,11 @@ namespace PruebaAPI
         public int Id { get; set; }
         public string Titulo { get; set; }
         public string Director { get; set; }
+        public string Descripcion { get; set; }
+        public bool EsEstreno { get; set; }
+        public string Genero { get; set; }
+        public bool EsAnimada { get; set; }
         public DateTime FechaEstreno { get; set; }
+        public string UrlVideo { get; set; }
     }
 }
